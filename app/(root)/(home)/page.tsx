8c650bf42b04d374cb449197ab482680bd5d5973
Dhwanilv26 <dhwanil2604@gmail.com>
2025-02-02 16:32:50 +0530

    implemented basic clerk

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
-const Home = () => {
-  return <div>Home</div>;
-};
-
-export default Home;
+export default function Home() {
+  return (
+    <div>
+      <UserButton />
+    </div>
+  );
+}
