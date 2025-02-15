17c60bf243d0365e2638fd0a64c504e6b71caaa6
Dhwanilv26 <dhwanil2604@gmail.com>
2025-02-16 00:50:43 +0530

    implemented the create answer action

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -9,7 +9,15 @@ import { Editor } from "@tinymce/tinymce-react";
 import { useTheme } from "@/context/ThemeProvider";
 import { Button } from "../ui/button";
 import Image from "next/image";
-const Answer = () => {
+import { createAnswer } from "@/lib/actions/answers.action";
+import { usePathname } from "next/navigation";
+
+interface Props {
+  question: string;
+  questionId: string;
+  authorId: string;
+}
+const Answer = ({ question, questionId, authorId }: Props) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const form = useForm<z.infer<typeof AnswerSchema>>({
     resolver: zodResolver(AnswerSchema),
@@ -18,10 +26,36 @@ const Answer = () => {
     },
   });
 
+  const pathname = usePathname();
+
   const editorRef = React.useRef(null);
   const { mode } = useTheme(); // or "dark" depending on your application context
 
-  const handleCreateAnswer = (data: z.infer<typeof AnswerSchema>) => {};
+  const handleCreateAnswer = async (values: z.infer<typeof AnswerSchema>) => {
+    setIsSubmitting(true);
+
+    try {
+      await createAnswer({
+        content: values.answer,
+        author: JSON.parse(authorId),
+        question: JSON.parse(questionId),
+        path: pathname,
+      });
+
+      form.reset();
+
+      if (editorRef.current) {
+        const editor = editorRef.current as any;
+
+        editor.setContent("");
+      }
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <div>
