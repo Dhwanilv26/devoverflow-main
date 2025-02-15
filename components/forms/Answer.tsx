fe6007faa78cb7a85e34cadd888a496a0c967022
Dhwanilv26 <dhwanil2604@gmail.com>
2025-02-16 00:23:00 +0530

    implemented the answer model

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -30,7 +30,10 @@ const Answer = () => {
           Write your answer here
         </h4>
 
-        <Button className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500">
+        <Button
+          className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
+          onClick={() => {}}
+        >
           <Image
             src="/assets/icons/stars.svg"
             width={12}
