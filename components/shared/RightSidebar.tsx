67255198eba9b66f2b8257480f89146cd53f8a6d
Dhwanilv26 <dhwanil2604@gmail.com>
2025-02-18 20:19:36 +0530

    implemented top questions feature

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,22 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
 
-const RightSidebar = () => {
-  const hotQuestions = [
-    { _id: "1", title: "Next js is the best" },
-    { _id: "2", title: "Next js is the best" },
-    { _id: "3", title: "Next js is the best" },
-    { _id: "4", title: "Next js is the best" },
-  ];
+const popularTags = [
+  { _id: "1", name: "javascript", totalQuestions: 100 },
+  { _id: "2", name: "typescript", totalQuestions: 200 },
+  { _id: "3", name: "aws", totalQuestions: 50 },
+  { _id: "4", name: "docker", totalQuestions: 150 },
+  { _id: "5", name: "llms", totalQuestions: 200 },
+];
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
 
-  const popularTags = [
-    { _id: "1", name: "javascript", totalQuestions: 100 },
-    { _id: "2", name: "typescript", totalQuestions: 200 },
-    { _id: "3", name: "aws", totalQuestions: 50 },
-    { _id: "4", name: "docker", totalQuestions: 150 },
-    { _id: "5", name: "llms", totalQuestions: 200 },
-  ];
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen flex-col  overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden w-[350px]">
       <div>
