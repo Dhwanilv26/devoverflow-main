d87cb4c4e0557b39d41d0d93c2a76daf45df4fa6
Dhwanilv26 <dhwanil2604@gmail.com>
2025-02-20 20:18:18 +0530

    implemented basic filter state for the home page

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,16 +1,46 @@
 "use client";
 import { HomePageFilters } from "@/constants/filters";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery } from "@/lib/utils";
 
 const HomeFilters = () => {
-  const active = "";
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [active, setActive] = useState("");
+
+  const handleTypeClick = (item: string) => {
+    if (active === item) {
+      setActive("");
+      // to toggle and reset the filters on double click
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: null,
+      });
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: item.toLowerCase(),
+      });
+
+      router.push(newUrl, { scroll: false });
+    }
+  };
+
   return (
     <div className="mt-10 flex-wrap gap-3 md:flex hidden">
       {HomePageFilters.map((item) => (
         <Button
           key={item.value}
-          onClick={() => {}}
+          onClickCapture={() => handleTypeClick(item.value)}
+          // onclickcapture is used to manipulate the data being sent before its being sent , unlike onclick
           className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
             active === item.value
               ? "dark:hover:bg-dark-400 bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500"
