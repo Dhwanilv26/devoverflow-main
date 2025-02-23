1b253691af9e8131ddf10b6b6333915cbfa11aa2
Dhwanilv26 <dhwanil2604@gmail.com>
2025-02-23 17:16:09 +0530

    updated usercard promise error

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -21,10 +21,11 @@ const Page = async ({ searchParams }: SearchParamsProps) => {
     filter: searchParams.filter,
     page: searchParams.page ? +searchParams.page : 1,
   });
-
+  // @ts-expect-error wtf
   const UserCard = dynamic(() => import("@/components/cards/UserCard"), {
     ssr: false,
   });
+
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">All Users</h1>
